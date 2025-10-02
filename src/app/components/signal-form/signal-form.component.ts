852458b5deca3208ae82c579a877e9de7ef40466
DvirMon <dvirmonajem@e-square.io>
2025-10-02 11:21:34 +0300

    move to angular and plat with forms

--- src/app/components/signal-form/signal-form.component.ts
+++ src/app/components/signal-form/signal-form.component.ts
@@ -1,18 +1,17 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { Control, disabled, form, schema } from '@angular/forms/signals';
+import { Control, disabled, form, required, schema } from '@angular/forms/signals';
 import { User } from '../../models/user.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-signal-form',
-  imports: [Control],
+  imports: [Control, JsonPipe],
   templateUrl: './signal-form.component.html',
   styleUrls: ['./signal-form.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignalFormComponent {
-
-
-  private readonly user = signal<User>({
+  protected readonly user = signal<User>({
     firstName: '',
     lastName: '',
     email: '',
@@ -33,64 +32,58 @@ export class SignalFormComponent {
   });
 
   protected formSchema = schema<User>((path) => {
-    disabled((path.firstName), ( {valueOf}) => valueOf(path.lastName) === 'Doe' );
-  })
+    required(path.firstName, { message: `is required` });
+    // required((path.firstName), ( {valueOf}) => valueOf(path.lastName) === 'Doe' );
+    disabled(path.firstName, ({ valueOf }) => valueOf(path.lastName) === 'chris');
+  });
 
-  protected userForm = form(this.user, () => ({
-    updateOn: 'blur',
-    validators: [],
-    asyncValidators: [],
-  }));
+  protected userForm = form(this.user, this.formSchema);
 
-//   protected readonly form = this.formService.createForm();
-//   protected readonly submitting = signal(false);
-//   protected readonly formData = signal<User | null>(null);
+  //   protected readonly form = this.formService.createForm();
+  //   protected readonly submitting = signal(false);
+  //   protected readonly formData = signal<User | null>(null);
 
   protected readonly notificationTypes = ['email', 'sms', 'push'] as const;
 
-    constructor() {
-        console.log(this.userForm.length);
-        console.log(this.userForm.name);
+  constructor() {
+  }
+
+  //   protected getErrorMessage(errors: ValidationErrors | null | undefined): string {
+  //     if (!errors) return '';
+  //     return this.formService.getErrorMessage(errors);
+  //   }
+
+  //   protected getFieldClass(errors: ValidationErrors | null | undefined): string {
+  //     return errors && Object.keys(errors).length > 0 ? 'error' : '';
+  //   }
+
+  //   protected isNotificationSelected(type: string): boolean {
+  //     const notifications = this.form.get('preferences.notifications')?.value as string[];
+  //     return notifications.includes(type);
+  //   }
+
+  //   protected onNotificationChange(event: Event): void {
+  //     const input = event.target as HTMLInputElement;
+  //     const notifications = this.form.get('preferences.notifications')?.value as string[];
+
+  //     if (input.checked) {
+  //       this.form.patchValue({
+  //         preferences: {
+  //           notifications: [...notifications, input.value],
+  //         },
+  //       });
+  //     } else {
+  //       this.form.patchValue({
+  //         preferences: {
+  //           notifications: notifications.filter((n) => n !== input.value),
+  //         },
+  //       });
+  //     }
+  //   }
+
+  protected async onSubmit(): Promise<void> {
+    if (this.userForm.email().valid()) {
+      // this.userForm.();
     }
-
-//   protected getErrorMessage(errors: ValidationErrors | null | undefined): string {
-//     if (!errors) return '';
-//     return this.formService.getErrorMessage(errors);
-//   }
-
-//   protected getFieldClass(errors: ValidationErrors | null | undefined): string {
-//     return errors && Object.keys(errors).length > 0 ? 'error' : '';
-//   }
-
-//   protected isNotificationSelected(type: string): boolean {
-//     const notifications = this.form.get('preferences.notifications')?.value as string[];
-//     return notifications.includes(type);
-//   }
-
-//   protected onNotificationChange(event: Event): void {
-//     const input = event.target as HTMLInputElement;
-//     const notifications = this.form.get('preferences.notifications')?.value as string[];
-
-//     if (input.checked) {
-//       this.form.patchValue({
-//         preferences: {
-//           notifications: [...notifications, input.value],
-//         },
-//       });
-//     } else {
-//       this.form.patchValue({
-//         preferences: {
-//           notifications: notifications.filter((n) => n !== input.value),
-//         },
-//       });
-//     }
-//   }
-
-    protected async onSubmit(): Promise<void> {
-
-        
-        if (this.userForm.email().valid()) {
-            // this.userForm.();
-        }
-    }
+  }
 }
